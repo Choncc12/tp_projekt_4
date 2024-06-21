781d48c4994bbfe6f304b2ba5b2b0a4a3910614b
RealK5 <53981116+RealK5@users.noreply.github.com>
2024-06-21 16:47:44 +0200

    Delete build directory
    I tak trzeba wygenerować na nowo na każdym urządzeniu

--- build/SDL/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/Choncc12/tp_projekt_4.git@71435a20d7e43931771b50693d431dc3b5d428ea"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
